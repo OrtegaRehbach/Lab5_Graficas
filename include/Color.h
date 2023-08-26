1590ce01df16e9f530a61a44b79a37b6c544968e
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-08-25 18:34:52 -0600

    Update Color.h
    Constructor for float values between 0 and 1, fixed type ambiguity issue

--- include/Color.h
+++ include/Color.h
@@ -11,11 +11,25 @@ struct Color {
     Uint8 blue;
     Uint8 alpha;
 
-    Color(Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255)
-        : red((r > 255) ? 255 : ((r < 0) ? 0 : r)),
-          green((g > 255) ? 255 : ((g < 0) ? 0 : g)),
-          blue((b > 255) ? 255 : ((b < 0) ? 0 : b)),
-          alpha((a > 255) ? 255 : ((a < 0) ? 0 : a)) {}
+    Color(int r, int g, int b, int a = 255) {
+        red =   static_cast<Uint8>((r > 255) ? 255 : ((r < 0) ? 0 : r));
+        green = static_cast<Uint8>((g > 255) ? 255 : ((g < 0) ? 0 : g));
+        blue =  static_cast<Uint8>((b > 255) ? 255 : ((b < 0) ? 0 : b));
+        alpha = static_cast<Uint8>((a > 255) ? 255 : ((a < 0) ? 0 : a));
+    }
+
+    Color(float r, float g, float b, float a = 1.0f)
+        : red(static_cast<Uint8>(r * 255.0f)),
+          green(static_cast<Uint8>(g * 255.0f)),
+          blue(static_cast<Uint8>(b * 255.0f)),
+          alpha(static_cast<Uint8>(a * 255.0f)) {
+    
+        red = (red > 255) ? 255 : ((red < 0) ? 0 : red);
+        green = (green > 255) ? 255 : ((green < 0) ? 0 : green);
+        blue = (blue > 255) ? 255 : ((blue < 0) ? 0 : blue);
+        alpha = (alpha > 255) ? 255 : ((alpha < 0) ? 0 : alpha);
+    }
+
 
     Color operator*(float scalar) const {
         Uint8 r = static_cast<Uint8>(red * scalar);
@@ -26,10 +40,11 @@ struct Color {
     }
 
     Color operator*(Color other) const {
-        Uint8 r = static_cast<Uint8>((red / 255)    * (other.red / 255)     * 255);
-        Uint8 g = static_cast<Uint8>((green / 255)  * (other.green / 255)   * 255);
-        Uint8 b = static_cast<Uint8>((blue / 255)   * (other.blue / 255)    * 255);
-        Uint8 a = static_cast<Uint8>((alpha / 255)  * (other.alpha / 255)   * 255);
+        Uint8 r = static_cast<Uint8>((red * other.red) / 255);
+        Uint8 g = static_cast<Uint8>((green * other.green) / 255);
+        Uint8 b = static_cast<Uint8>((blue * other.blue) / 255);
+        Uint8 a = static_cast<Uint8>((alpha * other.alpha) / 255);
+
         return Color(r, g, b, a);
     }
 
