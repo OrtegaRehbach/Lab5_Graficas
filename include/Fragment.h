ee674a749ca2047b3289748a6948af26062915e6
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-08-30 02:27:29 -0600

    Update Fragment.h
    Added color as Fragment property.

--- include/Fragment.h
+++ include/Fragment.h
@@ -1,19 +1,21 @@
 #pragma once
 
 #include <iostream>
+#include "Color.h"
 
 struct Fragment
 {
     float x;
     float y;
+    Color color;
 
     // Constructor
-    Fragment(float _x, float _y) 
-        : x(_x), y(_y) {}
+    Fragment(float _x, float _y, Color _color = Color(255, 255, 255)) 
+        : x(_x), y(_y), color(_color) {}
 
     // Overload the << operator for printing a Fragment
     friend std::ostream& operator<<(std::ostream& os, const Fragment& fragment) {
-        os << "Fragment(" << fragment.x << ", " << fragment.y << ")";
+        os << "Fragment(" << fragment.x << ", " << fragment.y << ") " << fragment.color;
         return os;
     }
 };
