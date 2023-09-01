11f1e51d310bfb18efeacbf52313410553b7d78d
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-09-01 01:32:41 -0600

    Update Shaders.cpp
    Changed the way the fragment is shaded and set the color to a light gray.

--- src/Shaders.cpp
+++ src/Shaders.cpp
@@ -25,10 +25,14 @@ Vertex vertexShader(const Vertex& vertex) {
 
 Fragment fragmentShader(const Fragment& fragment) {
     // Example: Assign a constant color to each fragment
-    // Color fragColor(255, 0, 0, 255); // Red color with full opacity
+    glm::vec3 fragmentPosition(fragment.x, fragment.y, fragment.z);
+    Color fragmentColor(128, 128, 128);
+
+    Fragment shadedFragment = Fragment(fragmentPosition, fragmentColor);
 
     // You can modify this function to implement more complex shading
     // based on the fragment's attributes (e.g., depth, interpolated normals, texture coordinates, etc.)
+    
 
-    return fragment;
+    return shadedFragment;
 }
